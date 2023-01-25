3fef51e63f61b157637f05d70c97eba4fd3d5465
enduringwriter <keithstateson@gmail.com>
2023-01-25 16:26:28 -0600

    revision

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,7 +1,14 @@
+-- List all the databases
 SHOW DATABASES;
+-- Select employees database
 USE employees;
+-- Show tables in employees database
 SHOW TABLES;
+-- Code that created the employees table
+SHOW CREATE TABLE employees;
+-- Explore the employees table. Use DESCRIBE or EXPLAIN
 DESCRIBE employees;
+EXPLAIN employees;
 -- data types include: int, date, and string (varchar and enum)
 -- Q6 Which table(s) do you think contain a numeric type column?
 -- A6 emp_no (which likely stands for employee number)
@@ -10,6 +17,9 @@ DESCRIBE employees;
 -- Q8 Which table(s) do you think contain a date type column?
 -- Q8 birth_date and hire_date
 DESCRIBE departments;
+DESCRIBE dept_manager;
 -- Q9 What is the relationship between the employees and the departments tables?
--- A9 employees is a db while departments is a table, and employees needs to be specified as it can be the db or the table
+/* A9 employees is a db while departments are tables,
+employees needs to be specified as it can be the db or the table,
+and all tables in departments have a common feature "dept_no" */
 SHOW CREATE TABLE dept_manager;
