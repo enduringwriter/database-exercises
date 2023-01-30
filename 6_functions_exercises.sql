d99978d063a092514791e5e2f4eb45c03ffd9b8f
enduringwriter <keithstateson@gmail.com>
2023-01-30 12:12:24 -0600

    revised exercise

--- 6_functions_exercises.sql
+++ 6_functions_exercises.sql
@@ -8,6 +8,7 @@ Note, the Answer is simply the command below the Question
 SHOW DATABASES; -- show databases
 USE employees; -- select database employees
 SELECT DATABASE(); -- every current database being used
+SHOW CREATE DATABASE employees; -- displays the command that created the database
 DESCRIBE employees; -- describe database shows 6 columns: emp_no, birth_date, first_name, last_name, hire_date
 SHOW TABLES; -- show tables within employees database
 SELECT * FROM employees LIMIT 1;
@@ -26,13 +27,29 @@ WHERE last_name LIKE 'E%E';
 -- Q4: Find all employees hired in the 90s and born on Christmas.
 -- Use datediff() function to find how many days they have been working at the company.
 -- Hint: You will also need to use NOW() or CURDATE().
-SELECT CONCAT(FORMAT(DATEDIFF(CURDATE(), hire_date), 'N'), ' days')
+SELECT hire_date, CONCAT(FORMAT(DATEDIFF(CURDATE(), hire_date), 'N'), ' days') as Days_at_Co
 FROM employees
 WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25';
 
 -- Q5: Find the smallest and largest current salary from the salaries table.
-SELECT CONCAT('$', FORMAT(MAX(salary), 'C')) FROM salaries; -- $158,220
-SELECT CONCAT('$', FORMAT(MIN(salary), 'C')) FROM salaries; -- $38,623
+-- Hint: "current" salary
+SELECT * FROM salaries LIMIT 1;
+
+-- get employees who are currently employed
+SELECT * FROM salaries;
+SELECT max(to_date) from salaries;
+
+-- another way to get employees who are currently employed
+SELECT * FROM salaries
+WHERE to_date > now();
+
+SELECT MIN(salary), MAX(salary)
+FROM salaries
+WHERE to_date = '9999-01-1'; -- min/max salary is $38,623/$158,220
+
+-- the following commands are all employees both formerly and currently employed
+SELECT CONCAT('$', FORMAT(MAX(salary), 'C')) FROM salaries;
+SELECT CONCAT('$', FORMAT(MIN(salary), 'C')) FROM salaries; 
 
 -- Q6: Use your knowledge of built in SQL functions to generate a username for all of the employees.
 --  A username should be all lowercase, and consist of the first character of the employees first name,
@@ -44,7 +61,7 @@ SELECT CONCAT(
 	SUBSTR(lower(first_name), 1, 1),
     SUBSTR(lower(last_name), 1, 4),
     '_',
-    SUBSTR(birth_date, 6, 2),
-    SUBSTR(year(birth_date), 3, 2)
+    LPAD(MONTH(birth_date), 2, 0), -- 2 characters, 0 padding
+    SUBSTR(birth_date, 3, 2)
 	)
 FROM employees;
