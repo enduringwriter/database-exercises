9ae64fd1b4ab524ea8506285436ee4632c2382b6
enduringwriter <keithstateson@gmail.com>
2023-02-01 16:18:18 -0600

    update join exercise

--- 8_join_exercises.sql
+++ 8_join_exercises.sql
@@ -59,7 +59,7 @@ WHERE titles.to_date LIKE '9999%' AND departments.dept_name = 'Customer Service'
 GROUP BY Title
 ORDER BY TITLE ASC;
 
--- Q5: Find the current salary of all current managers.
+-- Q5: Find the current salary of all current managers. Include the department each manager is in.
 SELECT departments.dept_name AS 'Department Name',
 	CONCAT(employees.first_name, ' ', employees.last_name) AS 'Name',
     salaries.salary AS 'Salary'
@@ -71,13 +71,45 @@ WHERE salaries.to_date LIKE '9999%' and dept_manager.to_date LIKE '9999%'
 ORDER BY departments.dept_name ASC;
 
 -- Q6: Find the number of current employees in each department.
+SELECT * FROM departments ORDER BY dept_no ASC;  -- shows 9 deparments
 
+SELECT departments.dept_no AS 'dept_no',
+	departments.dept_name AS 'dept_name',
+    COUNT(*) AS 'num_employees'
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_emp.to_date LIKE '9999%'
+GROUP BY departments.dept_no
+ORDER BY departments.dept_no ASC;
 
 -- Q7: Which department has the highest average salary? Hint: Use current not historic information.
 
+-- explore joined tables salaries and dept_emp
+SELECT * FROM salaries
+JOIN dept_emp USING (emp_no) -- USING (emp_no) is the same as: salaries.emp_no = dept_emp.emp_no
+LIMIT 10;
+
+SELECT DISTINCT departments.dept_name AS 'dept_name',
+	AVG(salaries.salary) AS 'average_salary'
+FROM salaries
+JOIN dept_emp ON salaries.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE salaries.to_date LIKE '9999%' AND dept_emp.to_date LIKE '9999%'
+GROUP BY departments.dept_name
+ORDER BY average_salary DESC LIMIT 1;
 
 -- Q8: Who is the highest paid employee in the Marketing department?
+SELECT DISTINCT dept_name FROM departments; -- Marketing
 
+SELECT employees.first_name AS first_name, employees.last_name AS last_name
+FROM salaries
+JOIN employees USING (emp_no)
+JOIN dept_emp USING (emp_no)
+JOIN departments USING (dept_no)
+WHERE departments.dept_name = 'Marketing'
+ORDER BY salaries.salary DESC
+Limit 1;
 
 -- Q9: Which current department manager has the highest salary?
 
